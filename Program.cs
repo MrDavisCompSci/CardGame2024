335ebf3f46271f96e7dba81211b4359c3b4808d2
MrDavisCompSci <38750430+MrDavisCompSci@users.noreply.github.com>
2024-07-01 11:22:04 +0000

    .

--- Program.cs
+++ Program.cs
@@ -5,6 +5,38 @@ class Program
     static void Main()
     {
         // Here is your empty program!
-        Console.WriteLine("Hello world!");
+        Console.WriteLine("Card Game");
+
+        // TEST: instantiate a card
+        Card card1 = new Card('D', 4);
+        // use the new card's Display method to, er, display it
+        card1.Display;    }
+}
+
+class Card
+{
+    private char suit;
+    private int rank;
+
+    public Card(char s, int r)
+    {
+        suit = s;
+        rank = r;
     }
+
+    public int GetSuit()
+    {
+        return suit;
+    }
+
+    public int GetRank()
+    {
+        return rank;
+    }
+
+    public void Display()
+    {  
+        Console.Write($"{rank} {suit}");
+    }
+    
 }
