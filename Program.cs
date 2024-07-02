c97f19cbea998248ee7e80911387cd66a4b46adb
MrDavisCompSci <38750430+MrDavisCompSci@users.noreply.github.com>
2024-07-02 08:14:49 +0000

    Added Deck class

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ class Program
         // Here is your empty program!
         Console.WriteLine("Card Game");
 
-        // TEST: instantiate a card
-        Card card1 = new Card('D', 4);
-        // use the new card's Display method to, er, display it
-        card1.Display()
-        ;    }
+        // TEST: instantiate a deck and display it
+        Deck myDeck = new Deck();
+        myDeck.Display();
+        Console.WriteLine();
+    }
 }
 
 class Card
@@ -39,7 +39,40 @@ class Card
 
     public void Display()
     {  
-        Console.Write($"{rank} {suit}");
+        Console.Write($"|{rank}{suit}|");
     }
     
+}
+
+class Deck
+{
+    // attribute
+    private Card[] cards;
+
+    // constructor
+    public Deck()
+    {
+        // instantiate a bunch of cards
+        char[] suits = new char[] {'D','S','C','H'};
+        cards = new Card[52];
+
+        int count = 0;
+
+        foreach (char suit in suits)
+        {
+            for (int rank = 1; rank <= 13; rank++)
+            {
+                cards[count] = new Card(suit, rank);
+                count++;
+            }
+        }
+    }
+
+    public void Display()
+    {
+        foreach(Card card in cards)
+        {
+            card.Display();
+        }   
+    }
 }
